010b46448e0de0f442b9e545512fec7b772c2012
Harishgowda22b05 <124109765+Harishgowda22b05@users.noreply.github.com>
2024-10-06 14:35:31 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 locals {
   region = "ap-south-1"
   name = "${terraform.workspace}-cluster"
-  vpc_cidr = "172.31.0/16"
+  vpc_cidr = "172.31.0/20"
   azs      = ["ap-south-1a", "ap-south-1b"]
   public_subnets = ["172.31.1.0/24", "172.31.2.0/24"]
   private_subnet = ["172.31.3.0", "172.31.4.0/24"]
